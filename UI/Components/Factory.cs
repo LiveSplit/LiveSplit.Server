d16312a8f59d50d1b06c098cdbf74f30c8592412
wooferzfg1 <spapushin@gmail.com>
2015-01-30 17:15:18 -0600

    changed version to 1.1

--- UI/Components/Factory.cs
+++ UI/Components/Factory.cs
@@ -44,12 +44,12 @@ namespace LiveSplit.UI.Components
 
         public Version Version
         {
-            get { return Version.Parse("1.0.0"); }
+            get { return Version.Parse("1.1.0"); }
         }
 
         public string XMLURL
         {
-            get { return "http://livesplit.org/update/Components/noupdates.xml"; }
+            get { return "http://livesplit.org/update/Components/LiveSplit.Server.xml"; }
         }    
     }
 }
