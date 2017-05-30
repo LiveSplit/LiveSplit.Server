83b4687c1841ca3040ca36f64d551d56600fd0b3
Sergey Papushin <spapushin@gmail.com>
2017-05-30 15:58:49 -0500

    Update version to 1.7

--- UI/Components/ServerFactory.cs
+++ UI/Components/ServerFactory.cs
@@ -20,7 +20,7 @@ namespace LiveSplit.UI.Components
 
         public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.6.3");
+        public Version Version => Version.Parse("1.7");
 
         public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Server.xml";
     }
