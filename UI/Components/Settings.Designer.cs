ed8a3f8a70543a2a445d37ea88a3b2dfaa417970
DJCrabhat <djcrabhat@sosimplerecords.com>
2020-05-07 15:41:49 -0700

    ui change to "Local IP(s)"

--- UI/Components/Settings.Designer.cs
+++ UI/Components/Settings.Designer.cs
@@ -40,7 +40,7 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 56F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 69F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
@@ -72,9 +72,9 @@
             // txtPort
             // 
             this.txtPort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtPort.Location = new System.Drawing.Point(59, 4);
+            this.txtPort.Location = new System.Drawing.Point(72, 4);
             this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(368, 20);
+            this.txtPort.Size = new System.Drawing.Size(355, 20);
             this.txtPort.TabIndex = 1;
             // 
             // label2
@@ -83,15 +83,16 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(3, 35);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.Size = new System.Drawing.Size(60, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Local IP:";
+            this.label2.Text = "Local IP(s):";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
             // 
             // label3
             // 
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(59, 35);
+            this.label3.Location = new System.Drawing.Point(72, 35);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 3;
@@ -100,10 +101,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(59, 57);
+            this.label4.Location = new System.Drawing.Point(72, 57);
             this.label4.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(364, 26);
+            this.label4.Size = new System.Drawing.Size(341, 26);
             this.label4.TabIndex = 4;
             this.label4.Text = "This is NOT your public IP. With most network setups, only devices on your networ" +
     "k can use this address to find your computer.";
